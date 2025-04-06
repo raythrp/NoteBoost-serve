9c774980bae0d2de3edc95a14213f2182d524848
Jonathan Wicaksono <112994348+JonathanNAW@users.noreply.github.com>
2025-04-06 19:37:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/auth", authRoutes);
+app.use("/", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
