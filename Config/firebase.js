7538b8e20300ab7dc0a13b8201826c4e4071a0de
Rayhan Athar <112993973+raythrp@users.noreply.github.com>
2025-04-12 15:40:18 +0700

    dynamic port & changed serviceaccount read method

--- Config/firebase.js
+++ Config/firebase.js
@@ -1,6 +1,7 @@
 const admin = require("firebase-admin");
+const fs = require('fs');
 require("dotenv").config();
-const serviceAccount = process.env.ENVIRONMENT == 'development' ? require("../noteboost-ff14a6993ce5.json") : require('/secrets/FIREBASE_KEY');
+const serviceAccount = process.env.ENVIRONMENT == 'development' ? require("../noteboost-ff14a6993ce5.json") : JSON.parse(fs.readFileSync('/secrets/FIREBASE_KEY', 'utf8'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
