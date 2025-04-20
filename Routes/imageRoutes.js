1bba75eccf7facedc4ea2ee34796e253b4e7307f
Jonathan N.A.W <jonathan22001@mail.unpad.ac.id>
2025-04-20 17:05:15 +0700

    Penambahan verify token pada imageRoutes.js

--- Routes/imageRoutes.js
+++ Routes/imageRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../Middleware/upload');
+const verifyToken = require('../Middleware/authMiddleware');
 const { extractTextFromImage } = require('../Controllers/imageControllers');
 
-router.post('/extract', upload.single('image'), extractTextFromImage);
+router.post('/extract', verifyToken, upload.single('image'), extractTextFromImage);
 
 module.exports = router;
