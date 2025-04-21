c81dde66613d93a86d23d113bca670295bce22aa
Jonathan N.A.W <jonathan22001@mail.unpad.ac.id>
2025-04-21 22:49:00 +0700

    Fix OCR langsung masuk database

--- Routes/imageRoutes.js
+++ Routes/imageRoutes.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const upload = require('../Middleware/upload');
 const verifyToken = require('../Middleware/authMiddleware');
-const { extractTextFromImage } = require('../Controllers/imageControllers');
+const {
+    extractTextFromImage,
+    extractTextAndSave
+  } = require('../Controllers/imageControllers');
 
-router.post('/extract', verifyToken, upload.single('image'), extractTextFromImage);
+router.post('/extract-and-save', verifyToken, upload.single('image'), extractTextAndSave);
 
 module.exports = router;
