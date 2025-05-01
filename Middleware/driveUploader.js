8a9653bc0aca0c3906b38bbdd5a6c8658d219125
Stevanus Felixiano <stevanusf77@gnail.com>
2025-05-01 19:36:47 +0700

    link foto

--- Middleware/driveUploader.js
+++ Middleware/driveUploader.js
@@ -40,6 +40,7 @@ async function uploadToDrive(file) {
         mimeType: file.mimetype,
         body: bufferStream,
       },
+      fields: 'id, thumbnailLink, webContentLink, webViewLink',
     });
 
     console.log("✅ Google Drive upload response:", response.data);
@@ -49,7 +50,7 @@ async function uploadToDrive(file) {
       requestBody: { role: "reader", type: "anyone" },
     });
 
-    const link = `https://drive.google.com/uc?export=view&id=${response.data.id}`;
+    const link = response.data.thumbnailLink || response.data.webContentLink;
 
     console.log("🌍 Public link:", link);
     return link;
