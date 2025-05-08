2507c1311bd42c24eaa43af6c8d848b5a7396d23
Stevanus Felixiano <stevanusf77@gnail.com>
2025-05-08 20:21:08 +0700

    Securing

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { login, register, logout, updateUserInfo, updateJenjang, forgotPassword, resetPassword, checkTokenExpiry} = require("../Controllers/authControllers");
+const { login, register, logout, updateUserInfo, updateJenjang, forgotPassword, resetPassword, checkTokenExpiry, forgotPasswordProtected, forgotPasswordPublic} = require("../Controllers/authControllers");
 const verifyToken = require("../Middleware/authMiddleware");
 
 const router = express.Router();
@@ -9,7 +9,7 @@ router.post("/register", register);
 router.post("/logout", logout);
 router.post("/update-user", verifyToken, updateUserInfo);
 router.post("/update-jenjang", verifyToken, updateJenjang);  
-router.post("/forgot-password", verifyToken, forgotPassword);
-router.post("/reset-password", resetPassword);
+router.post("/forgot-password", verifyToken, forgotPasswordProtected);
+router.post("/reset-password", forgotPasswordPublic);
 router.post("/check", verifyToken ,checkTokenExpiry);
 module.exports = router;
