266177f33217ab4672c18b15974a853b0d47f308
Rayhan Athar <112993973+raythrp@users.noreply.github.com>
2025-05-14 23:48:05 +0700

    cors added for new frontend server(sg)

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use(cors({
     "http://localhost:8080",
     "https://noteboost-772262781875.asia-southeast2.run.app",
     "https://noteboost-sg-772262781875.asia-southeast1.run.app",
+    "https://www.noteboost.site"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
